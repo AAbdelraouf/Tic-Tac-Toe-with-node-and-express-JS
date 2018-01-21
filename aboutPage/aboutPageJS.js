98e3d99fbf21aa7225841f5adbb5c71a27eb46f3
Ahmed Abdelraouf <edbraouf@gmail.com>
2018-01-21 13:17:31 -0800

    new update

--- aboutPage/aboutPageJS.js
+++ aboutPage/aboutPageJS.js
@@ -1,5 +1,5 @@
 $(document).ready(function()
-{       
+{   console.log('welcome!')
     // About the game button //
     $('#theAboutButton').on('click', function(){
         location.href = 'https://tictactogame.herokuapp.com/'
